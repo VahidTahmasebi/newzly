4a09657113ccd1a428621c6c7122e5c1068cb369
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-20 11:43:17 +0330

    Initial program files

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    disableStaticImages: true,
+    domains: ["t2.gstatic.com"],
+  },
+};
+
+module.exports = nextConfig;
