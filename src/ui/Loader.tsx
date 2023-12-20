a1af469d12a924075c5e4565d3468851dc5f62aa
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-20 21:35:27 +0330

    Add loader component

---
+++ src/ui/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center bg-red-500">
+      <p>Loading Data...</p>
+    </div>
+  );
+};
+
+export default Loader;
